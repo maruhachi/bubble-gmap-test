4688545761d7776f74e9d6fd4ac521471b5895aa
maruhachi <konchan1129@gmail.com>
2021-02-20 10:12:41 +0900

    GMap制御スクリプト追加.

--- test2.js
+++ test2.js
@@ -1,9 +1,16 @@
-alert("テストですよ!!!")
+console.log("test2.js");
 
-function tete(){
-	console.log("見てる？")
-}
-
-console.log("動いてくれ");
-
-tete();
+var mapOptions = {
+  zoom: 15, // 拡大倍率
+  center: { lat: 35.45216977054224, lng: 138.7300826551757 }, // 緯度・経度
+};
+// マップオブジェクト作成
+var map = new google.maps.Map(
+  document.getElementById("map"), // マップを表示する要素
+  mapOptions // マップオプション
+);
+//　マップにマーカーを表示する
+var marker = new google.maps.Marker({
+  map: map, // 対象の地図オブジェクト
+  position: { lat: 35.45216977054224, lng: 138.7300826551757 }, // 緯度・経度
+});
